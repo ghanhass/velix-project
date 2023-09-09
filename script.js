efd6346cca9f992fb7dfb04605385c705c71fb99
Hassen Ghanmi <hassoon@BULLSEYE-debian-live-builder>
2023-09-09 15:53:10 +0100

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+function createRipple(event) {
+    const button = event.currentTarget;
+    if(!button.classList.contains("disabled")){
+        const circle = document.createElement("span");
+        const diameter = Math.max(button.clientWidth, button.clientHeight);
+        const radius = diameter / 2;
+        
+        circle.style.width = circle.style.height = `${diameter}px`;
+        circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+        circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+        circle.classList.add("ripple");
+        
+        const ripple = button.getElementsByClassName("ripple")[0];
+        
+        if (ripple) {
+          ripple.remove();
+        }
+    
+        button.appendChild(circle);
+    }
+}
+
+window.onload = ()=>{
+    var navbarItems = document.querySelectorAll("header nav .nav-item");
+    navbarItems.forEach((navItem)=>{
+        navItem.addEventListener("click", createRipple);
+    })
+}
